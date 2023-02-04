23eedf38ece3648e869bc92ec9a30cf128fb88a4
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-02-04 09:22:23 +0000

    Updating packages and implementation

--- src/sync-func/Program.cs
+++ src/sync-func/Program.cs
@@ -29,12 +29,13 @@ var host = new HostBuilder()
     {
         var config = context.Configuration;
 
-        services.AddRepositoryApiClient(options => new RepositoryApiClientOptions(
-            config["apim_base_url"] ?? config["repository_base_url"] ?? throw new ArgumentNullException("apim_base_url"),
-            config["portal_repository_apim_subscription_key"] ?? throw new ArgumentNullException("portal_repository_apim_subscription_key"),
-            config["repository_api_application_audience"] ?? throw new ArgumentNullException("repository_api_application_audience"),
-            config["repository_api_path_prefix"] ?? "repository")
-        );
+        services.AddRepositoryApiClient(options =>
+        {
+            options.BaseUrl = config["apim_base_url"] ?? config["repository_base_url"] ?? throw new ArgumentNullException("apim_base_url");
+            options.ApiKey = config["portal_repository_apim_subscription_key"] ?? throw new ArgumentNullException("portal_repository_apim_subscription_key");
+            options.ApiAudience = config["repository_api_application_audience"] ?? throw new ArgumentNullException("repository_api_application_audience");
+            options.ApiPathPrefix = config["repository_api_path_prefix"] ?? "repository";
+        });
 
         services.AddMapRedirectRepository(options =>
         {
