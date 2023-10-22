6df1c10e678717663b2f0bc1b323f676bec2fc10
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-10-22 21:09:08 +0100

    Update handling of app insights connection

--- terraform/function_app.tf
+++ terraform/function_app.tf
@@ -24,6 +24,9 @@ resource "azurerm_linux_function_app" "app" {
       dotnet_version              = "7.0"
     }
 
+    application_insights_connection_string = azurerm_application_insights.ai.connection_string
+    application_insights_key = azurerm_application_insights.ai.instrumentation_key
+
     ftps_state          = "Disabled"
     always_on           = true
     minimum_tls_version = "1.2"
@@ -32,8 +35,6 @@ resource "azurerm_linux_function_app" "app" {
   app_settings = {
     "READ_ONLY_MODE"                             = var.environment == "prd" ? "true" : "false"
     "WEBSITE_RUN_FROM_PACKAGE"                   = "1"
-    "APPINSIGHTS_INSTRUMENTATIONKEY"             = azurerm_application_insights.ai.instrumentation_key
-    "APPLICATIONINSIGHTS_CONNECTION_STRING"      = azurerm_application_insights.ai.connection_string
     "ApplicationInsightsAgent_EXTENSION_VERSION" = "~3"
     "apim_base_url"                              = data.azurerm_api_management.platform.gateway_url
     "portal_repository_apim_subscription_key"    = format("@Microsoft.KeyVault(VaultName=%s;SecretName=%s)", azurerm_key_vault.kv.name, azurerm_key_vault_secret.repository_api_subscription_secret.name)
