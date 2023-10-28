2c1e16486089db6c2da1a4aaa49d7aacf480517b
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-10-28 08:38:48 +0100

    Fix for subscription creation

--- terraform/api_subscriptions.tf
+++ terraform/api_subscriptions.tf
@@ -4,9 +4,9 @@ resource "azurerm_api_management_subscription" "repository_api_subscription" {
   api_management_name = data.azurerm_api_management.platform.name
   resource_group_name = data.azurerm_api_management.platform.resource_group_name
 
-  state = "active"
+  state         = "active"
   allow_tracing = false
 
-  api_id       = data.azurerm_api_management_api.repository_api.id
+  api_id       = split(";", data.azurerm_api_management_api.repository_api.id)[0] // Strip revision from id when creating subscription
   display_name = format("%s-%s", local.function_app_name, data.azurerm_api_management_api.repository_api.name)
 }
