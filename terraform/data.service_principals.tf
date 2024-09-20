728f7301a2c0ee54d8fe1a1bf6d00e1136df201a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-20 19:15:16 +0100

    Adding missing resource reference

--- terraform/data.service_principals.tf
+++ terraform/data.service_principals.tf
@@ -1,3 +1,7 @@
 data "azuread_service_principal" "repository_api" {
   display_name = var.repository_api.application_name
 }
+
+data "azuread_service_principal" "servers_integration_api" {
+  display_name = var.servers_integration_api.application_name
+}
