f63d68fe97ceb14dd354076990472e98b0bed7be
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-09-20 18:59:18 +0100

    Adding missing role assignment

--- terraform/function_app_role_assignments.tf
+++ terraform/function_app_role_assignments.tf
@@ -3,3 +3,9 @@ resource "azuread_app_role_assignment" "repository_api" {
   principal_object_id = azurerm_linux_function_app.app.identity.0.principal_id
   resource_object_id  = data.azuread_service_principal.repository_api.object_id
 }
+
+resource "azuread_app_role_assignment" "servers_integration_api" {
+  app_role_id         = data.azuread_service_principal.servers_integration_api.app_roles[index(data.azuread_service_principal.servers_integration_api.app_roles.*.display_name, "ServiceAccount")].id
+  principal_object_id = azurerm_linux_function_app.app.identity.0.principal_id
+  resource_object_id  = data.azuread_service_principal.servers_integration_api.object_id
+}
