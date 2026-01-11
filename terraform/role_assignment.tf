e778d37efb9f39178fa969044475bdada536ce79
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-01-11 18:25:12 +0000

    Add role assignment for app data storage with appropriate permissions

--- terraform/role_assignment.tf
+++ terraform/role_assignment.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "app-to-storage" {
   role_definition_name = "Storage Blob Data Owner"
   principal_id         = local.sync_funcapp_identity.principal_id
 }
+
+resource "azurerm_role_assignment" "app-to-app-data-storage" {
+  scope                = azurerm_storage_account.app_data_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = local.sync_funcapp_identity.principal_id
+}
