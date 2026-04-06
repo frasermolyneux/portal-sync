3df9d22cb8e46c6185431ef2e53a2e898d9c3dd7
Fraser Molyneux <frasermolyneux@mx-mail.io>
2026-04-06 19:57:22 +0100

    refactor: remove unused dependencies and simplify BanFileMonitor constructor

--- src/XtremeIdiots.Portal.Sync.App.Tests/Functions/BanFileMonitorTests.cs
+++ src/XtremeIdiots.Portal.Sync.App.Tests/Functions/BanFileMonitorTests.cs
@@ -1,14 +1,8 @@
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Moq;
-using MX.Api.Abstractions;
 using XtremeIdiots.Portal.Repository.Abstractions.Constants.V1;
-using XtremeIdiots.Portal.Repository.Abstractions.Models.V1.BanFileMonitors;
-using XtremeIdiots.Portal.Repository.Api.Client.V1;
-using BanFileMonitorOrder = XtremeIdiots.Portal.Repository.Abstractions.Constants.V1.BanFileMonitorOrder;
-using XtremeIdiots.Portal.Sync.App.Helpers;
 using XtremeIdiots.Portal.Sync.App.Interfaces;
 
 namespace XtremeIdiots.Portal.Sync.App.Tests.Functions;
@@ -16,18 +10,12 @@ namespace XtremeIdiots.Portal.Sync.App.Tests.Functions;
 public class BanFileMonitorTests
 {
     private readonly Mock<ILogger<BanFileMonitor>> _loggerMock = new();
-    private readonly Mock<IFtpHelper> _ftpHelperMock = new();
-    private readonly Mock<IBanFileIngest> _banFileIngestMock = new();
     private readonly Mock<IBanFilesRepository> _banFilesRepositoryMock = new();
-    private readonly Mock<IRepositoryApiClient> _repositoryApiClientMock = new(MockBehavior.Loose) { DefaultValue = DefaultValue.Mock };
     private readonly TelemetryClient _telemetryClient = new(new TelemetryConfiguration());
 
     private BanFileMonitor CreateSut() => new(
         _loggerMock.Object,
-        _ftpHelperMock.Object,
-        _banFileIngestMock.Object,
         _banFilesRepositoryMock.Object,
-        _repositoryApiClientMock.Object,
         _telemetryClient);
 
     [Fact]
@@ -42,34 +30,7 @@ public class BanFileMonitorTests
     {
         Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
             null!,
-            _ftpHelperMock.Object,
-            _banFileIngestMock.Object,
             _banFilesRepositoryMock.Object,
-            _repositoryApiClientMock.Object,
-            _telemetryClient));
-    }
-
-    [Fact]
-    public void Constructor_NullFtpHelper_ThrowsArgumentNullException()
-    {
-        Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
-            _loggerMock.Object,
-            null!,
-            _banFileIngestMock.Object,
-            _banFilesRepositoryMock.Object,
-            _repositoryApiClientMock.Object,
-            _telemetryClient));
-    }
-
-    [Fact]
-    public void Constructor_NullBanFileIngest_ThrowsArgumentNullException()
-    {
-        Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
-            _loggerMock.Object,
-            _ftpHelperMock.Object,
-            null!,
-            _banFilesRepositoryMock.Object,
-            _repositoryApiClientMock.Object,
             _telemetryClient));
     }
 
@@ -78,21 +39,6 @@ public class BanFileMonitorTests
     {
         Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
             _loggerMock.Object,
-            _ftpHelperMock.Object,
-            _banFileIngestMock.Object,
-            null!,
-            _repositoryApiClientMock.Object,
-            _telemetryClient));
-    }
-
-    [Fact]
-    public void Constructor_NullRepositoryApiClient_ThrowsArgumentNullException()
-    {
-        Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
-            _loggerMock.Object,
-            _ftpHelperMock.Object,
-            _banFileIngestMock.Object,
-            _banFilesRepositoryMock.Object,
             null!,
             _telemetryClient));
     }
@@ -102,41 +48,8 @@ public class BanFileMonitorTests
     {
         Assert.Throws<ArgumentNullException>(() => new BanFileMonitor(
             _loggerMock.Object,
-            _ftpHelperMock.Object,
-            _banFileIngestMock.Object,
             _banFilesRepositoryMock.Object,
-            _repositoryApiClientMock.Object,
             null!));
-    }
-
-    [Fact]
-    public async Task ImportLatestBanFiles_WhenApiReturnsFailure_ThrowsApplicationException()
-    {
-        Mock.Get(_repositoryApiClientMock.Object.BanFileMonitors.V1)
-            .Setup(x => x.GetBanFileMonitors(It.IsAny<GameType[]?>(), It.IsAny<Guid[]?>(), It.IsAny<Guid?>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BanFileMonitorOrder?>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new ApiResult<CollectionModel<BanFileMonitorDto>>(System.Net.HttpStatusCode.InternalServerError));
-
-        var sut = CreateSut();
-
-        await Assert.ThrowsAsync<ApplicationException>(() => sut.ImportLatestBanFiles(null));
-    }
-
-    [Fact]
-    public async Task ImportLatestBanFiles_WhenApiReturnsEmptyList_CompletesSuccessfully()
-    {
-        var response = new ApiResponse<CollectionModel<BanFileMonitorDto>>(
-            new CollectionModel<BanFileMonitorDto>(new List<BanFileMonitorDto>()));
-
-        Mock.Get(_repositoryApiClientMock.Object.BanFileMonitors.V1)
-            .Setup(x => x.GetBanFileMonitors(It.IsAny<GameType[]?>(), It.IsAny<Guid[]?>(), It.IsAny<Guid?>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BanFileMonitorOrder?>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new ApiResult<CollectionModel<BanFileMonitorDto>>(System.Net.HttpStatusCode.OK, response));
-
-        var sut = CreateSut();
-
-        await sut.ImportLatestBanFiles(null);
-
-        Mock.Get(_repositoryApiClientMock.Object.BanFileMonitors.V1)
-            .Verify(x => x.GetBanFileMonitors(It.IsAny<GameType[]?>(), It.IsAny<Guid[]?>(), It.IsAny<Guid?>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<BanFileMonitorOrder?>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 
     [Fact]
--- src/XtremeIdiots.Portal.Sync.App/BanFileMonitor.cs
+++ src/XtremeIdiots.Portal.Sync.App/BanFileMonitor.cs
@@ -1,14 +1,9 @@
-using System.Diagnostics;
-
 using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
 using XtremeIdiots.Portal.Repository.Abstractions.Constants.V1;
-using XtremeIdiots.Portal.Repository.Abstractions.Models.V1.BanFileMonitors;
-using XtremeIdiots.Portal.Repository.Api.Client.V1;
-using XtremeIdiots.Portal.Sync.App.Helpers;
 using XtremeIdiots.Portal.Sync.App.Interfaces;
 using XtremeIdiots.Portal.Sync.App.Telemetry;
 
@@ -16,145 +11,12 @@ namespace XtremeIdiots.Portal.Sync.App;
 
 public class BanFileMonitor(
     ILogger<BanFileMonitor> logger,
-    IFtpHelper ftpHelper,
-    IBanFileIngest banFileIngest,
     IBanFilesRepository banFilesRepository,
-    IRepositoryApiClient repositoryApiClient,
     TelemetryClient telemetryClient)
 {
     private readonly ILogger<BanFileMonitor> logger = logger ?? throw new ArgumentNullException(nameof(logger));
-    private readonly IFtpHelper ftpHelper = ftpHelper ?? throw new ArgumentNullException(nameof(ftpHelper));
-    private readonly IBanFileIngest banFileIngest = banFileIngest ?? throw new ArgumentNullException(nameof(banFileIngest));
     private readonly IBanFilesRepository banFilesRepository = banFilesRepository ?? throw new ArgumentNullException(nameof(banFilesRepository));
-    private readonly IRepositoryApiClient repositoryApiClient = repositoryApiClient ?? throw new ArgumentNullException(nameof(repositoryApiClient));
     private readonly TelemetryClient telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
-
-    // DISABLED: Replaced by portal-server-agent ban file watcher + portal-server-events ban processor
-    // [Function(nameof(ImportLatestBanFilesManual))]
-    // public async Task ImportLatestBanFilesManual([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
-    // {
-    //     await ImportLatestBanFiles(null).ConfigureAwait(false);
-    // }
-
-    // DISABLED: Replaced by portal-server-agent ban file watcher + portal-server-events ban processor
-    // [Function(nameof(ImportLatestBanFiles))]
-    public async Task ImportLatestBanFiles(TimerInfo? myTimer)
-    {
-        await ScheduledJobTelemetry.ExecuteWithTelemetry(
-            telemetryClient,
-            nameof(ImportLatestBanFiles),
-            async () =>
-            {
-                var banFileMonitorsApiResponse = await repositoryApiClient.BanFileMonitors.V1.GetBanFileMonitors(null, null, null, 0, 50, null).ConfigureAwait(false);
-
-                if (!banFileMonitorsApiResponse.IsSuccess || banFileMonitorsApiResponse.Result?.Data?.Items is null)
-                {
-                    logger.LogCritical("Failed to retrieve ban file monitors from the repository");
-                    throw new ApplicationException("Failed to retrieve ban file monitors from the repository");
-                }
-
-                await ProcessBanFileMonitors(banFileMonitorsApiResponse.Result.Data.Items).ConfigureAwait(false);
-            }).ConfigureAwait(false);
-    }
-
-    private async Task ProcessBanFileMonitors(IEnumerable<BanFileMonitorDto> banFileMonitors)
-    {
-        foreach (var banFileMonitorDto in banFileMonitors)
-        {
-            if (banFileMonitorDto.GameServer is null)
-                continue;
-
-            if (string.IsNullOrWhiteSpace(banFileMonitorDto.GameServer.FtpHostname)
-                || string.IsNullOrWhiteSpace(banFileMonitorDto.GameServer.FtpUsername)
-                || string.IsNullOrWhiteSpace(banFileMonitorDto.GameServer.FtpPassword)
-                || banFileMonitorDto.GameServer.FtpPort is null)
-                continue;
-
-            try
-            {
-                var remoteFileSize = await ftpHelper.GetFileSize(
-                banFileMonitorDto.GameServer.FtpHostname,
-                banFileMonitorDto.GameServer.FtpPort.Value,
-                banFileMonitorDto.FilePath,
-                banFileMonitorDto.GameServer.FtpUsername,
-                banFileMonitorDto.GameServer.FtpPassword,
-                banFileMonitorDto.TelemetryProperties).ConfigureAwait(false);
-                var banFileSize = await banFilesRepository.GetBanFileSizeForGame(banFileMonitorDto.GameServer.GameType).ConfigureAwait(false);
-
-                if (remoteFileSize is null)
-                {
-                    telemetryClient.TrackEvent("BanFileInit", banFileMonitorDto.TelemetryProperties);
-
-                    await using (var banFileStream = await banFilesRepository.GetBanFileForGame(banFileMonitorDto.GameServer.GameType).ConfigureAwait(false))
-                    {
-                        await ftpHelper.UpdateRemoteFileFromStream(
-                            banFileMonitorDto.GameServer.FtpHostname,
-                            banFileMonitorDto.GameServer.FtpPort.Value,
-                            banFileMonitorDto.FilePath,
-                            banFileMonitorDto.GameServer.FtpUsername,
-                            banFileMonitorDto.GameServer.FtpPassword,
-                            banFileStream,
-                            banFileMonitorDto.TelemetryProperties).ConfigureAwait(false);
-                    }
-
-                    var editBanFileMonitorDto = new EditBanFileMonitorDto(banFileMonitorDto.BanFileMonitorId, banFileSize, DateTime.UtcNow);
-                    await repositoryApiClient.BanFileMonitors.V1.UpdateBanFileMonitor(editBanFileMonitorDto).ConfigureAwait(false);
-                    continue;
-                }
-
-                if (remoteFileSize != banFileMonitorDto.RemoteFileSize)
-                {
-                    telemetryClient.TrackEvent("BanFileChangedOnRemote", banFileMonitorDto.TelemetryProperties);
-
-                    var remoteBanFileData = await ftpHelper.GetRemoteFileData(
-                        banFileMonitorDto.GameServer.FtpHostname,
-                        banFileMonitorDto.GameServer.FtpPort.Value,
-                        banFileMonitorDto.FilePath,
-                        banFileMonitorDto.GameServer.FtpUsername,
-                        banFileMonitorDto.GameServer.FtpPassword,
-                        banFileMonitorDto.TelemetryProperties).ConfigureAwait(false);
-
-                    await banFileIngest.IngestBanFileDataForGame(banFileMonitorDto.GameServer.GameType.ToString(), remoteBanFileData).ConfigureAwait(false);
-
-                    var editBanFileMonitorDto = new EditBanFileMonitorDto(banFileMonitorDto.BanFileMonitorId, (long)remoteFileSize, DateTime.UtcNow);
-                    await repositoryApiClient.BanFileMonitors.V1.UpdateBanFileMonitor(editBanFileMonitorDto).ConfigureAwait(false);
-                }
-
-                if (remoteFileSize != banFileSize && remoteFileSize == banFileMonitorDto.RemoteFileSize)
-                {
-                    telemetryClient.TrackEvent("BanFileChangedOnSource", banFileMonitorDto.TelemetryProperties);
-
-                    await using (var banFileStream = await banFilesRepository.GetBanFileForGame(banFileMonitorDto.GameServer.GameType).ConfigureAwait(false))
-                    {
-                        await ftpHelper.UpdateRemoteFileFromStream(
-                            banFileMonitorDto.GameServer.FtpHostname,
-                            banFileMonitorDto.GameServer.FtpPort.Value,
-                            banFileMonitorDto.FilePath,
-                            banFileMonitorDto.GameServer.FtpUsername,
-                            banFileMonitorDto.GameServer.FtpPassword,
-                            banFileStream,
-                            banFileMonitorDto.TelemetryProperties).ConfigureAwait(false);
-                    }
-
-                    var editBanFileMonitorDto = new EditBanFileMonitorDto(banFileMonitorDto.BanFileMonitorId, banFileSize, DateTime.UtcNow);
-                    await repositoryApiClient.BanFileMonitors.V1.UpdateBanFileMonitor(editBanFileMonitorDto).ConfigureAwait(false);
-                }
-
-                if (remoteFileSize == banFileMonitorDto.RemoteFileSize)
-                {
-                    var editBanFileMonitorDto = new EditBanFileMonitorDto(banFileMonitorDto.BanFileMonitorId)
-                    {
-                        LastSync = DateTime.UtcNow
-                    };
-                    await repositoryApiClient.BanFileMonitors.V1.UpdateBanFileMonitor(editBanFileMonitorDto).ConfigureAwait(false);
-                }
-            }
-            catch (Exception ex)
-            {
-                telemetryClient.TrackException(ex, banFileMonitorDto.TelemetryProperties);
-            }
-        }
-    }
 
     [Function(nameof(GenerateLatestBansFileManual))]
     public async Task GenerateLatestBansFileManual([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
